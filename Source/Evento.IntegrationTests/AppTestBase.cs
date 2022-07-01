740ac4d10e0e54fab9036e25578e9172d98a13b0
Yury Pliner <yury.pliner@gmail.com>
2022-07-01 18:47:17 +0300

    Fix

--- Source/Evento.IntegrationTests/AppTestBase.cs
+++ Source/Evento.IntegrationTests/AppTestBase.cs
@@ -4,8 +4,6 @@ using DotNet.Testcontainers.Containers.Configurations.MessageBrokers;
 using DotNet.Testcontainers.Containers.Modules.Abstractions;
 using DotNet.Testcontainers.Containers.Modules.Databases;
 using DotNet.Testcontainers.Containers.Modules.MessageBrokers;
-using EasyNetQ.Management.Client;
-using EasyNetQ.Management.Client.Model;
 using Xunit;
 
 namespace Evento.IntegrationTests;
@@ -17,7 +15,7 @@ public class AppTestBase : IAsyncLifetime
             new RabbitMqTestcontainerConfiguration("rabbitmq:3.10-management")
             {
                 Username = "guest",
-                Password = "guest"
+                Password = "guest",
             }
         )
         .Build();
@@ -42,46 +40,11 @@ public class AppTestBase : IAsyncLifetime
 
     public async Task InitializeAsync()
     {
-        await rmqContainer.StartAsync();
-        await pgContainer.StartAsync();
-
-        using var rmqManagementClient = new ManagementClient(rmqContainer.Hostname, "guest", "guest", rmqContainer.Port);
-        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(20));
-
-        await WaitForRabbitMqReadyAsync(rmqManagementClient, cts.Token);
-    }
-    
-    private static async Task WaitForRabbitMqReadyAsync(ManagementClient managementClient, CancellationToken cancellationToken)
-    {
-        while (true)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            if (await IsRabbitMqReadyAsync(managementClient, cancellationToken))
-                return;
-
-            await Task.Delay(500, cancellationToken);
-        }
-    }
-
-    private static async Task<bool> IsRabbitMqReadyAsync(ManagementClient managementClient, CancellationToken cancellationToken)
-    {
-        try
-        {
-            return await managementClient.IsAliveAsync(new Vhost{Name = "/"}, cancellationToken);
-        }
-        catch (OperationCanceledException)
-        {
-            throw;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
+        await Task.WhenAll(rmqContainer.StartAsync(), pgContainer.StartAsync());
     }
 
     public async Task DisposeAsync()
     {
-        await rmqContainer.StopAsync();
-        await pgContainer.StopAsync();
+        await Task.WhenAll(rmqContainer.StopAsync(), pgContainer.StopAsync());
     }
 }
